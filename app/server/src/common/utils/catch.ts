dddab1634aa6099f209ed83070b045319a45428c
wangly19 <18607948581@163.com>
2021-12-08 17:15:30 +0800

    delete(bugs): 删除\b未知字符开头的catch文件❌

--- app/server/src/common/utils/catch.ts
+++
@@ -1,8 +0,0 @@
-import { HttpException } from "@nestjs/common";
-import { HttpCode } from 'src/common/enums/http'
-
-export class HttpError extends HttpException {
-  constructor (code: HttpCode) {
-    super(null, code)
-  }
-}
