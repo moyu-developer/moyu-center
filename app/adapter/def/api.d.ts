fdfa18d500bc1942ff68cf4c016c33b208903caf
wangly19 <18607948581@163.com>
2022-01-19 00:20:31 +0800

    feat(adapter): 适配器调整

--- app/adapter/def/api.d.ts
+++ app/adapter/def/api.d.ts
@@ -1,17 +1,39 @@
-export interface ReturnAPIModule {
+interface BaseParamsType {
+  name: string;
+  desc?: string;
+  required: string;
+  type?: string;
+  example?: string
+}
 
-  path: string;
+
+export interface ReturnAPIModule {
   method: string;
   title: string;
-  tag?: string[];
+  desc?: string;
+  catname: string;
+  tag: string[];
+  path: string;
+  req_params: BaseParamsType[]
+  req_body_form: BaseParamsType[]
+  req_headers: Record<string, any>;
+  req_query: BaseParamsType[];
+  req_body_type: string;
+  res_body_type: string;
+  res_body_is_json_schema: boolean,
+  res_body: string;
   [K: string]: any
 }
 
 export interface ReturnInterfaceResult {
-  apis: any[];
+  apis: ReturnAPIModule[];
   category: {
     name: string,
     description?: string
+    externalDocs?: {
+      description: string,
+      url: string
+    }
   }[];
   prefix?: string;
-}
+} 
---
+++ app/adapter/type.d.ts
@@ -0,0 +1,4 @@
+declare module "swagger-client" {
+  const fun: any
+  export = fun
+}
